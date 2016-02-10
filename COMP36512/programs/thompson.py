33a5616dd14637702e43e33ed3d1f61395cd3916
Todd Davies <todd434@gmail.com>
2016-02-10 17:58:17 +0000

    Finished Thompson's algorithm implementation

---
+++ COMP36512/programs/thompson.py
@@ -0,0 +1,84 @@
+"""
+Basic implementation of Thompson's NFA -> DFA algorithm
+"""
+
+from pprint import pprint
+
+nfa = {
+  0 : {"": {1,7}},
+  1 : {"": {2,4}},
+  4 : {"b": {5}},
+  2 : {"a": {3}},
+  3 : {"": {6}},
+  5 : {"": {6}},
+  6 : {"": {7,1}},
+  7 : {"a": {8}},
+  8 : {"b": {9}},
+  9 : {"b": {10}},
+  10: {}
+}
+
+accepting = {10}
+
+def accept_nfa(nfa, acceptingStates, inputStr, currentState=0):
+  if inputStr == None or len(inputStr) == 0:
+    return currentState in acceptingStates
+  char = inputStr[0]
+  transitions = nfa[currentState][char] if char in nfa[currentState] else {}
+  epsilons = nfa[currentState][""]  if "" in nfa[currentState] else {}
+  for nextState in transitions:
+    if accept_nfa(nfa, acceptingStates, inputStr[1:], nextState):
+      return True
+  for nextState in epsilons:
+    if accept_nfa(nfa, acceptingStates, inputStr, nextState):
+      return True
+  return False
+
+def e_closure(nfa, states):
+  out = set(states)
+  for state in states:
+    immediateClosure = nfa[state][""] if "" in nfa[state] else set()
+    neighbourClosures = set()
+    for s in immediateClosure:
+      neighbourClosures = neighbourClosures.union(e_closure(nfa, [s]))
+    out = out.union(immediateClosure.union(neighbourClosures))
+  return out
+
+def move(nfa, states, transition):
+  out = set()
+  for state in states:
+    if transition in nfa[state]:
+      out = out.union(nfa[state][transition])
+  return out
+
+def mapping(input_set, names):
+  if repr(input_set) not in names:
+    current = max(names.values() + [-1]) + 1
+    names[repr(input_set)] = current
+  return chr(ord('a') + names[repr(input_set)])
+
+def nfa_subset(nfa, initial):
+  names = {}
+  output = {}
+  input_symbols = map(lambda x: nfa[x].keys(), nfa.keys())
+  input_symbols = set(reduce(lambda x, y: x+y, input_symbols))
+  input_symbols.remove("")
+  DStates = [e_closure(nfa, initial)]
+  marked = []
+  unmarked = filter(lambda x: x not in marked, DStates)
+  while len(unmarked) > 0:
+    t = unmarked.pop()
+    marked.append(t)
+    for symbol in input_symbols:
+      u = e_closure(nfa, move(nfa, t, symbol))
+      if u not in DStates:
+        DStates.append(u)
+      if symbol != "" and not t == set():
+        if mapping(t, names) in output:
+          output[mapping(t, names)][symbol] = mapping(u, names)
+        else:
+          output[mapping(t, names)] = {symbol:mapping(u, names)}
+    unmarked = filter(lambda x: x not in marked, DStates)
+  return output
+
+pprint(nfa_subset(nfa, [0]))
